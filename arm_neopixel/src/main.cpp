d35cd3ae3a631e7326cfd59938201bb7185d6fa2
hato9 <tomohiko2005tori@gmail.com>
2023-12-22 22:52:03 +0900

    目標角を入れれば光るようになった

--- arm_neopixel/src/main.cpp
+++ arm_neopixel/src/main.cpp
@@ -2,76 +2,70 @@
 #include <Adafruit_NeoPixel.h>
 
 #define arm1_PIN PA6
-#define arm2_PIN PA5
 #define numpixels 24
 #define led_count 12
-#define light_blur 60
+#define led_brightness 255
+#define blur 3
+
+HardwareSerial Serial1(PA10, PA9);
 
 Adafruit_NeoPixel pixels1(numpixels, arm1_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel pixels2(numpixels, arm2_PIN, NEO_GRB + NEO_KHZ800);
 
-int calc_led_angle(int led_id);
-byte calc_delta_angle(int target_center, int target_halfwidth, int led_angle);
-int calc_brightness(int start_angle, int end_angle, int led_id);
+void calc_led_vector(int led_id, double led_vector[]);
+void calc_target_vector(int target_center, double target_vector[]);
+void calc_cossim(double led_vector[], double target_vector[], double *cossim);
+int calc_diff(double cossim);
 
 void setup()
 {
   pixels1.begin();
-  pixels2.begin();
+  Serial1.begin(115200);
   delay(1000);
 }
 
 void loop()
 {
-  // for (int i = 0; i < numpixels; i++)
-  // {
-  //   pixels1.setPixelColor(i, pixels1.Color(250, 250, 250));
-  //   pixels1.show();
-  //   pixels2.setPixelColor(i, pixels2.Color(250, 250, 250));
-  //   pixels2.show();
-  // }
-  // for (int j = 0; j < 360; j++)
-  // {
-  //   for (int i = 0; i < led_count; i++)
-  //   {
-  //     arm1_pixels.setPixelColor(i, arm1_pixels.Color(calc_brightness(j, j, i), calc_brightness(j, j, i), calc_brightness(j, j, i)));
-  //     arm1_pixels.show();
-  //     arm1_pixels.setPixelColor(i + 12, arm1_pixels.Color(calc_brightness(j, j, i), calc_brightness(j, j, i), calc_brightness(j, j, i)));
-  //     arm1_pixels.show();
-  //   }
-  // }
-}
-
-void calc_led_vector(int led_id, double* led_vector[2])
-{
-  int degree = 15 + led_id * (360 / led_count);
-  *led_vector[0] = sin(degree);
-  *led_vector[1] = cos(degree);
-}
-
-byte calc_delta_angle(int target_center, int target_halfwidth, int led_angle)
-{
-  if (target_center - target_halfwidth <= led_angle && led_angle <= target_center + target_halfwidth)
+  double led_vector[] = {0, 0};
+  double target_vector[] = {0, 0};
+  double cossim;
+  for (int i = 0; i < 360; i++)
   {
-    return 1;
-  }
-  else
-  {
-    return 0;
+    for (int j = 12; j < 24; j++)
+    {
+      calc_led_vector(j, led_vector);
+      calc_target_vector(i, target_vector);
+      calc_cossim(led_vector, target_vector, &cossim);
+      pixels1.setPixelColor(j, pixels1.Color(calc_diff(cossim), calc_diff(cossim), calc_diff(cossim)));
+    }
+    pixels1.show();
+    delay(10);
   }
 }
 
-int calc_brightness(int target_center, int target_width, int led_id)
+void calc_led_vector(int led_id, double *led_vector)
 {
-  int led_angle = calc_led_angle(led_id);
-  bool delta_angle = calc_delta_angle(start_angle, end_angle, led_angle);
-  if (delta_angle < 0)
+  double degree = (15 + led_id * (360 / led_count)) * 2 * PI / 360;
+  led_vector[0] = sin(degree);
+  led_vector[1] = cos(degree);
+}
+
+void calc_target_vector(int target_center, double *target_vector)
+{
+  double degree = target_center * 2 * PI / 360;
+  target_vector[0] = sin(degree);
+  target_vector[1] = cos(degree);
+}
+
+void calc_cossim(double *led_vector, double *target_vector, double *cossim)
+{
+  *cossim = ((led_vector[0] * target_vector[0] + led_vector[1] * target_vector[1]) + 1) / 2;
+};
+
+int calc_diff(double cossim)
+{
+  if((cossim - 1) * blur + 1 > 0)
   {
-    return 255;
-  }
-  else if (255 * (1 - delta_angle / light_blur) >= 0)
-  {
-    return (int)(64 * (1 - delta_angle / light_blur));
+    return (int)(((cossim - 1) * blur + 1) * led_brightness);
   }
   else
   {
