84f894e331fc7d3e0076b42e221ac9891d3a3578
Jun-robot <62178981+Jun-robot@users.noreply.github.com>
2023-12-23 23:02:27 +0900

    送受信できた組み合わせ

--- TweLite/zig_receive/zig_receive.cpp
+++ TweLite/zig_receive/zig_receive.cpp
@@ -1,118 +1,99 @@
-/*
-Zigbeeを受信して
-F446に送信する
-*/
-
+// use twelite mwx c++ template library
 #include <TWELITE>
 #include <NWK_SIMPLE>
 
-//通信
+#define CNT_MAX 1000
+
+/*** Config part */
 const uint32_t APP_ID = 0x1234abcd;
 const uint8_t CHANNEL = 13;
-const char APP_FOURCHAR[] = "TECH";
+const uint8_t PIN_DI1 = mwx::PIN_DIGITAL::DIO10; 
+const uint8_t PIN_DO1 = mwx::PIN_DIGITAL::DIO9; 
 
-//関数
+// application use
+int16_t cnt1ms;
+uint32_t ts;
+bool rcvflg;
+const char APP_FOURCHAR[] = "TECH";
+uint8_t u8devid = 0;
+
+/*** function prototype */
 void receive();
 
-//読み取るときのやつ
-uint8_t re_to_adrs; //送信先のアドレス 自分のアドレス
-uint8_t re_from_adrs; //送信元のアドレス
+/*** setup procedure (run once at cold boot) */
+void setup() {
 
-uint8_t re_data[4] = {0,0,0,0}; //送信データ
+    the_twelite
+	<< TWENET::appid(APP_ID) 
+	<< TWENET::channel(CHANNEL) 
+	<< TWENET::rx_when_idle(); 
 
-//ピン
-// const uint8_t DIP0= mwx::PIN_DIGITAL::DIO13; //adrs1
-// const uint8_t DIP1= mwx::PIN_DIGITAL::DIO16; //adrs2
-// const uint8_t LED0= mwx::PIN_DIGITAL::DIO9; 
-// const uint8_t LED1= mwx::PIN_DIGITAL::DIO8; 
+    auto&& nwksmpl = the_twelite.network.use<NWK_SIMPLE>();
+	   nwksmpl << NWK_SIMPLE::logical_id(0xFE); // set Logical ID. (0x00 means parent device)
 
-uint8_t my_adrs = 0xFE; //自分のアドレス
-
-void setup() {	
-	//ーーーーーID読み取りーーーーー
-	// pinMode(DIP0,INPUT);
-	// pinMode(DIP1,INPUT);
-	// pinMode(LED0,OUTPUT);
-	// pinMode(LED1,OUTPUT);
-	// delay(10);
-	// my_adrs += 0xA0;//子機のアドレスを計算
-	// my_adrs += digitalRead(DIP1) << 1;//子機のアドレスを計算
-	// my_adrs += digitalRead(DIP0); //子機のアドレスを計算
-	// digitalWrite(LED0,digitalRead(DIP0));
-	// digitalWrite(LED1,digitalRead(DIP1));
-	
-	//ーーーーーm無線通信設定ーーーーー
-	the_twelite
-		<< TWENET::appid(APP_ID) 
-		<< TWENET::channel(CHANNEL) 
-		<< TWENET::rx_when_idle(); 
-	auto&& 	nwksmpl = the_twelite.network.use<NWK_SIMPLE>();
-					nwksmpl << NWK_SIMPLE::logical_id(my_adrs); //0x00→マスター 0xFE→スレーブ
-	the_twelite.begin(); // start twelite!
-
-	// //ーーーーーー有線通信設定ーーーーーー
-	// Serial1.setup(192, 192); // 64byte TX, 192byte RX
-	// Serial1.begin(115200);
-
-	//ーーーーー起動ーーーーー
-	Serial << "--- TweLite->F446 ---" << mwx::crlf;
+    the_twelite.begin(); // start twelite!
+    Serial << "--- Parent act ---" << mwx::crlf;
 }
 
-int16_t mytimer = -1;
-
+/*** loop procedure (called every event) */
 void loop() {
-	//ーーーーー受信ーーーーー
-	while (the_twelite.receiver.available()) { //受信待ち 受信したデータがなくなるまで繰り返す
-		Serial << "kita";
-		receive();
-		Serial 	<< "re_fr:" << format("0x%X", re_from_adrs) //送信元のアドレス 16進数で表示してます
-						<< "  data: " << (int)re_data[0]
-						<< " : " << (int)re_data[1]
-						<< " : " << (int)re_data[2]
-						<< " : " << (int)re_data[3]
-						// << " : " << (int)mytimer
-						<< mwx::crlf << mwx::flush;
+
+    if(TickTimer.available()){
+	++cnt1ms;
+    }
+
+    while (the_twelite.receiver.available()) {
+	receive();
+	rcvflg = true;
+    }
+
+    if( cnt1ms >= CNT_MAX){
+	cnt1ms -= CNT_MAX;
+
+	ts = (ts + 1 ) % 100000;
+
+	if(rcvflg){
+	    rcvflg = false;
+	}else{
+	    // Serial << "ts:" <<  int(ts) << mwx::crlf << mwx::flush;;
 	}
-
-		//ーーーーーF446送信ーーーーー
-		// Serial1 << (byte)250; //スタートビット
-		// for(int i=0;i<4;i++){
-		// 	Serial1 << (byte)re_data[i];
-		// }
-		// Serial1 << mwx::flush;
-
-	
-	// if(TickTimer.available()){ //1msごとに実行
-	// 	mytimer++;
-	// }
-	//ーーーーーF446送信ーーーーー
-	// if(mytimer >= 100){
-			// mytimer = 0;
-		// }
+    }
 }
-
 /*add function recive()*/
 void receive() {
-	auto&& rx = the_twelite.receiver.read(); //受信値をRXに読み込む
-	char fourchars[5]{}; // 判定用
-	auto&& np = expand_bytes(
-								rx.get_payload().begin(), 
-								rx.get_payload().end(), 
-								make_pair((uint8_t*)fourchars, 4) //fourcharsに4バイトの文字列を格納
-							);
+    uint8_t adrs;
+    uint16_t vcc;
+    uint16_t temp;
+    double temp2;
+    double vcc2;
 
-	if (strncmp(APP_FOURCHAR, fourchars, 4)) {
-		return;
-	}
-	expand_bytes(
-		np,
-		rx.get_payload().end(),
-		re_from_adrs, //uint8_t
-		re_data[0], //uint8_t
-		re_data[1], //uint8_t
-		re_data[2], //uint8_t
-		re_data[3]  //uint8_t
-	);
+		uint8_t hoge[4]; // init all elements as default (0).
+   
+    auto&& rx = the_twelite.receiver.read();
+    char fourchars[5]{}; // init all elements as default (0).
+    auto&& np = expand_bytes(
+			rx.get_payload().begin(), rx.get_payload().end()
+		, make_pair((uint8_t*)fourchars, 4)  // 4bytes of msg
+                );
+
+    if (strncmp(APP_FOURCHAR, fourchars, 4)) {return; } //文字列チェック
+
+    expand_bytes(	np,
+									rx.get_payload().end(),
+									adrs,
+									hoge[0],
+									hoge[1],
+									hoge[2],
+									hoge[3]
+								);
+    temp2 = (double)( temp - 500 ) / 10; //電圧値から温度換算
+    vcc2 = (double)vcc/1000; //電圧値から電源電圧換算
+    Serial 	<< "ts:" << int(ts) 
+						<< "：" << int(rx.get_addr_src_lid())
+						<< " adrs: " << (int)adrs
+						<< " : " << (int)hoge[0]
+						<< " : " << (int)hoge[1]
+						<< " : " << (int)hoge[2]
+						<< " : " << (int)hoge[3]
+						<< mwx::crlf << mwx::flush;
 }
-
-
