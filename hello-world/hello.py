c8f216ca0881740eb588a3d45374ed4d8b65f4d4
Jun-robot <saijun721.1@gmail.com>
2024-01-06 23:36:13 +0900

    これ入れとけば動きます

--- hello-world/hello.py
+++ hello-world/hello.py
@@ -10,17 +10,8 @@ import pygame
 
 print('Hello, World!')
 
-def alarm():
-    pygame.mixer.init(frequency = 44100)    # 初期設定
-    pygame.mixer.music.load("World2.mp3")     # 音楽ファイルの読み込み
-    pygame.mixer.music.play(1)              # 音楽の再生回数(1回)
-    # while(1):
-    #     a = input("Finish? --->")
-    #     if(a is 'y'): break
-    time.sleep(3)
-    pygame.mixer.music.stop()               # 再生の終了
-    return 0
-
+pygame.mixer.init(frequency = 44100)    # 初期設定
+pygame.mixer.music.load("World2.mp3")     # 音楽ファイルの読み込み
 
 
 def auto_detect_serial_port():
@@ -32,20 +23,45 @@ def auto_detect_serial_port():
             return p.device
     return None
 
-alarm()
+# alarm()
 
 auto_detect_serial_port()
 # ser = serial.Serial(auto_detect_serial_port(), 115200, timeout=0.1)
-ser = serial.Serial("/dev/ttyUSB0", 115200, timeout=0.1)
+ser = serial.Serial("/dev/ttyUSB1", 115200, timeout=0.0001)
 
-time.sleep(5)
+ser.close()
+ser.open()
+ser.reset_input_buffer()
 
+time.sleep(3)
+print("dd")
+
+n = 0
+music_flg = 0
 while True:
 	ser.write(b"1")
+	# receive = ser.readline()
 	time.sleep(.05)
+
 	ser.write(b"0")
+	receive = ser.readline()
 	time.sleep(.05)
-	print("aaaaaaaaaaaa")
+
+	receive = ser.readline()
+	
+	receive = receive.strip()
+	receive = receive.decode("utf-8")
+	print(n)
+	print(receive)
+	n += 1
+	if receive == 'D' and music_flg == 0:
+		music_flg = 1
+		n = 0
+		pygame.mixer.music.play(1)              # 音楽の再生回数(1回)
+	if receive == 'E':
+		pygame.mixer.music.stop()               # 再生の終了
+		music_flg = 0
+		n = 0
 
 
 
