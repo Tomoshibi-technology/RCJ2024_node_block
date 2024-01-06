b774e0867204571d2663d241fb13e60689ad9dc9
Jun-robot <saijun721.1@gmail.com>
2024-01-06 20:29:38 +0900

    ittan

--- hello-world/hello.py
+++ hello-world/hello.py
@@ -1,13 +1,27 @@
 import serial
 import time
 import serial.tools.list_ports
+import pygame
 
-from playsound import playsound
+# from playsound import playsound
 
 
 # playsound("World2.mp3")
 
 print('Hello, World!')
+
+def alarm():
+    pygame.mixer.init(frequency = 44100)    # 初期設定
+    pygame.mixer.music.load("World2.mp3")     # 音楽ファイルの読み込み
+    pygame.mixer.music.play(1)              # 音楽の再生回数(1回)
+    # while(1):
+    #     a = input("Finish? --->")
+    #     if(a is 'y'): break
+    time.sleep(3)
+    pygame.mixer.music.stop()               # 再生の終了
+    return 0
+
+
 
 def auto_detect_serial_port():
     ports = list(serial.tools.list_ports.comports())
@@ -18,6 +32,9 @@ def auto_detect_serial_port():
             return p.device
     return None
 
+alarm()
+
+auto_detect_serial_port()
 # ser = serial.Serial(auto_detect_serial_port(), 115200, timeout=0.1)
 ser = serial.Serial("/dev/ttyUSB0", 115200, timeout=0.1)
 
